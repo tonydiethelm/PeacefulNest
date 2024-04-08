2d27a4c71cef0bfb5efb7eb06f11dfaa4e67ee6a
tonydiethelm <tonydiethelm@gmail.com>
2024-04-08 12:28:05 -0700

    Added content for services page.

---
+++ src/routes/services/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+    import nursing from '../../lib/assets/nursing.jpg';
+</script>
+
+<main>
+    <section>
+        <img class="right" src={nursing} alt="Picture of a nursing baby.">
+        <p>I support families during the first 12 weeks after welcoming a new baby, 
+            so that family members can experience more joy, more confidence, and 
+            more peace</p>
+    </section>
+
+    <section>
+        <aside>
+            <div><p>Postpartum Support $35/hr</p>
+                <p>(16 hrs min.)</p>
+            </div>
+            <div>
+                <p>Ways to Pay for Postpartum Doula Care</p>
+                <ul>
+                    <li>Private Pay</li>
+                    <li>HSA or FSA</li>
+                    <li>Carrot Fertility Benefits</li>
+                    <li>Company Benefits</li>
+                    <li>Gift Certificate</li>
+                </ul>
+            </div>
+            <div>
+                <p>Peaceful Nest, LLC donates 5% of all 
+                    profits to <a href="www.gatewaydoulagroup.org/unitydoulavillage">
+                        Unity Doula Village</a> and 
+                    <a href="https://communitydoulaalliance.com">Community Doula Alliance</a>. Please check 
+                        out these amazing organizations!
+                </p>
+            </div>
+        </aside>
+        <p>As a postpartum doula I can...</p> 
+        <ul>
+            <li>provide non-medical, physical, emotional, educational and family support 
+                after the birth of your baby</li>
+            <li>assist you with self-care recovery measures</li>
+            <li>provide information and education about baby care</li>
+            <li>assist you in caring for and feeding your new baby</li>
+            <li>offer basic lactation support</li>
+            <li>assist your family with adjusting to new roles</li>
+            <li>pick up items from the pharmacy or store</li>
+            <li>offer household support such as preparing occasional light meals, 
+                sweeping, grocery shopping, laundry, tidying and dish-washing so you can nap, 
+                shower, or attend to other self-care tasks</li>
+            <li>give foot rubs  :)</li>
+            <li>prepare a hot pack or draw a bath</li>
+        </ul> 
+
+        
+    </section>
+</main>
